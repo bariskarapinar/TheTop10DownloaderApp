0ba4e3dcbf5a8bb1e135b592a9ef0a3a531df8f0
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-21 19:57:14 +0300

    Download the RSS Feed

--- app/src/main/java/com/gamebit/thetop10downloaderapp/MainActivity.kt
+++ app/src/main/java/com/gamebit/thetop10downloaderapp/MainActivity.kt
@@ -3,6 +3,15 @@ package com.gamebit.thetop10downloaderapp
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,19 +19,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val downloadData = DownloadData()
+        downloadData.execute("URL...")
     }
 
-    private inner class DownloadData : AsyncTask<String, Void, String>(){
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+            }
+
+            override fun doInBackground(vararg url: String?): String {
+
+
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.d("doInBackground", "Error Downloading")
+                }
+                return rssFeed
+            }
         }
+    }
 
-        override fun doInBackground(vararg p0: String?): String {
+    private fun downloadXML(urlPath : String?): String{
+        val xmlResult = StringBuilder()
+        try {
+            val url = URL(urlPath)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val response = connection.responseCode
+            Log.d("downloadXML", "responseCode$response")
 
-
-            TODO("Not yet implemented")
+            val inputStream = connection.inputStream
+            val inputStreamReader = InputStreamReader(inputStream)
+            val reader = BufferedReader(inputStreamReader)
+        }catch (e: MalformedURLException) {
+            e.printStackTrace()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }catch (e: Exception){
+            e.printStackTrace()
         }
 
 
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
