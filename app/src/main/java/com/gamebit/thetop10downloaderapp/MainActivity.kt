61d70867f6fddcd82b8c5828a785e6b20b033797
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-21 20:19:24 +0300

    Reading the Data

--- app/src/main/java/com/gamebit/thetop10downloaderapp/MainActivity.kt
+++ app/src/main/java/com/gamebit/thetop10downloaderapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val downloadData = DownloadData()
-        downloadData.execute("URL...")
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
     }
 
     companion object {
@@ -30,8 +30,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun doInBackground(vararg url: String?): String {
-
-
                 val rssFeed = downloadXML(url[0])
                 if (rssFeed.isEmpty()) {
                     Log.d("doInBackground", "Error Downloading")
@@ -39,28 +37,35 @@ class MainActivity : AppCompatActivity() {
                 return rssFeed
             }
         }
-    }
 
-    private fun downloadXML(urlPath : String?): String{
-        val xmlResult = StringBuilder()
-        try {
-            val url = URL(urlPath)
-            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-            val response = connection.responseCode
-            Log.d("downloadXML", "responseCode$response")
+        private fun downloadXML(urlPath : String?): String{
+            val xmlResult = StringBuilder()
+            try {
+                val url = URL(urlPath)
+                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                val response = connection.responseCode
+                Log.d("downloadXML", "responseCode$response")
 
-            val inputStream = connection.inputStream
-            val inputStreamReader = InputStreamReader(inputStream)
-            val reader = BufferedReader(inputStreamReader)
-        }catch (e: MalformedURLException) {
-            e.printStackTrace()
-        }catch (e: IOException){
-            e.printStackTrace()
-        }catch (e: Exception){
-            e.printStackTrace()
+                val inputStream = connection.inputStream
+                val inputStreamReader = InputStreamReader(inputStream)
+                val reader = BufferedReader(inputStreamReader)
+
+                val inputBuffer = CharArray(500)
+                var charsRead = 0
+                while (charsRead >= 0) {
+                    charsRead = reader.read(inputBuffer)
+                    if (charsRead > 0) {
+                        xmlResult.append(String(inputBuffer, 0, charsRead))
+                    }
+                }
+                reader.close()
+                Log.d("Received", "${xmlResult.length} bytes")
+                return xmlResult.toString()
+            }catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return "" // If it gets to here, return an empty string
         }
-
-
     }
 
 
