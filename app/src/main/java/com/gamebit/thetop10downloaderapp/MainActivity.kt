a37f5d8125ca845ccaf7c8fe75a34ae4b57604e2
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-21 19:40:22 +0300

    AsyncTask

--- app/src/main/java/com/gamebit/thetop10downloaderapp/MainActivity.kt
+++ app/src/main/java/com/gamebit/thetop10downloaderapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.gamebit.thetop10downloaderapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private inner class DownloadData : AsyncTask<String, Void, String>(){
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg p0: String?): String {
+
+
+            TODO("Not yet implemented")
+        }
+
+
     }
 }
