d603a4e5bb512ee2db88292b77e53657e171e1b7
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-24 19:17:30 +0300

    Improving the Adapter

--- app/src/main/java/com/gamebit/thetop10downloaderapp/FeedAdapter.kt
+++ app/src/main/java/com/gamebit/thetop10downloaderapp/FeedAdapter.kt
@@ -22,7 +22,12 @@ class FeedAdapter(context: Context, private val resource: Int, private val appli
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         Log.d(TAG, "getView() called")
-        val view = inflater.inflate(resource, parent, false)
+        val view: View
+        if (convertView == null) {
+            view = inflater.inflate(resource, parent, false)
+        }    else {
+            view = convertView
+        }
 
         val tvName: TextView = view.findViewById(R.id.tvName)
         val tvArtist: TextView = view.findViewById(R.id.tvArtist)
